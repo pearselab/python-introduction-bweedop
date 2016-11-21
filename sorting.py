459ab9827b0a4715797a8278af8b09e720c1b6fc
bweedop <bodie.weedop@aggiemail.usu.edu>
2016-11-21 09:21:30 -0700

    11/21

--- sorting.py
+++ sorting.py
@@ -1,15 +1,23 @@
 bb_vec = [8,2,4,10,6]
 
-def bubble (bad_list):
-    length = len(bad_list) - 1
-    sorted = False
-
-    while not sorted:
-        sorted = True
-        for i in range(length):
-            if bad_list[i] > bad_list[i+1]:
-                sorted = False
-                bad_list[i], bad_list[i+1] = bad_list[i+1], bad_list[i]
-    return(bad_list)
-
-print(bubble(bb_vec))
+def bubble (dan, order):
+    foo = len(dan) - 1
+    bb_sort = False
+    if order == 0:
+        while not bb_sort:
+            bb_sort = True
+            for i in range(foo):
+                if dan[i]>dan[i+1]:
+                    bb_sort = False
+                    dan[i], dan[i+1] = dan[i+1], dan[i]
+        return(dan)
+    if order == 1:
+        while not bb_sort:
+            bb_sort = True
+            for i in range(foo):
+                if dan[i]<dan[i+1]:
+                    bb_sort = False
+                    dan[i], dan[i+1] = dan[i+1], dan[i]
+        return(dan)
+print(bubble(bb_vec, 1))
+#order = 0 if ascending order and 1 if descending order is wanted.
